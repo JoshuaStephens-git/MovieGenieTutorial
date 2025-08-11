bf40d4f0b5e30fa7e04a6471380f81424f05e626
Josh <joshua.stephens@live.com>
2025-08-11 18:01:57 -0500

    feat(controller): Add HomeController to serve homepage

---
+++ src/main/java/com/joshua/MovieGenieTutorial/HomeController.java
@@ -0,0 +1,19 @@
+package com.joshua.MovieGenieTutorial;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    public HomeController() {
+    }
+
+@GetMapping("/")
+public String homeRedirect() {
+    return "redirect:/home";
+}
+@GetMapping("/home")
+    public String home() {
+        return "homeHTML";
+    }
+}
