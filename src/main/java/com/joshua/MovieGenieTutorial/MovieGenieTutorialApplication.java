5a2f06ef9489489c7a92ced9b02692f35063c50e
Josh <joshua.stephens@live.com>
2025-08-11 15:22:16 -0500

    Initial commit

---
+++ src/main/java/com/joshua/MovieGenieTutorial/MovieGenieTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.joshua.MovieGenieTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieGenieTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieGenieTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joshua/MovieGenieTutorial/MovieGenieTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshua.MovieGenieTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieGenieTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
